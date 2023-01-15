271d0878ddbf452fb8f29810a0d2739e75cf8863
Cloud <101927226+Cloudipy@users.noreply.github.com>
2023-01-15 21:37:37 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+# run : screen python3 main.py
+import aiocron
+from pyrogram import Client
+
+print('start . . .')
+
+
+app = Client(
+    name='pyrogram',
+    api_id=0,            # CHANGE API_ID 
+    api_hash='api_id',   # CHANGE API_HASH
+    bot_token='TOKEN'    # CHANGE BOT TOKEN
+)
+
+USER = 0 # USER ID , @userinfobot GET USER ID
+FILE = '/etc/x-ui/x-ui.db'
+
+@aiocron.crontab('*/8 * * *')
+async def sendfile():
+    await app.send_document(USER,FILE)
+
+
+if __name__ == '__main__':
+    sendfile.start()
+    sendfile.stop()
+
+
+app.run()
+# Q&A : @Cloudipy
